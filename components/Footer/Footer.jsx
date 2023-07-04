73789d7077166b50424b3287b4a70371b69ebfb8
M0mahmoud <houdmohamed85@gmail.com>
2023-07-04 05:36:24 +0300

    Header & Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div>&#169;2023 Digital Products All rights reserved.</div>
+      <div className={styles.social}>
+        <Link href={"/"}>
+          <Image src="/1.svg" width={25} height={25} alt="Footer Icon" />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/2.svg" width={25} height={25} alt="Footer Icon" />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/3.svg" width={25} height={25} alt="Footer Icon" />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/4.svg" width={25} height={25} alt="Footer Icon" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { links } from "./NavLinks";
+import style from "./navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <div className={style.container}>
+        <Link href={"/"} className={style.logo}>
+          Blog
+        </Link>
+        <div
+          className={isMenuOpen ? `${style.links} ${style.open}` : style.links}
+        >
+          {links.map((link) => (
+            <Link key={link.id} href={link.url} className={style.link}>
+              {link.title}
+            </Link>
+          ))}
+          <button className={style.logout} onClick={() => {}}>
+            Logout
+          </button>
+        </div>
+        <button className={style.menuButton} onClick={handleMenuToggle}>
+          Menu
+        </button>
+      </div>
+      <div
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+        className={isMenuOpen ? style.overlay : ""}
+      />
+    </>
+  );
 };
 
 export default Navbar;
