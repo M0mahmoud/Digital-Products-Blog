74c5f39b39c0c66d4eba19fcf16bc7eb39f32c0e
M0mahmoud <houdmohamed85@gmail.com>
2023-07-07 01:48:48 +0300

    About Page

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className={styles.container}>
+      <div>Page</div>
+    </div>
+  );
 };
 
 export default Page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+import Hero from "public/hero.png";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Setup Project...</h1>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </main>
-  )
+  );
 }
